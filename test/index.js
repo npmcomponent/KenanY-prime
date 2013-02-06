78b0d732b968958966e7328de1fa584f2b63f3ff
Kenan Yildirim <kenan@kenany.me>
2013-02-06 16:53:24 -0500

    Add space between `function()` and opening bracket

--- test/index.js
+++ test/index.js
@@ -1,14 +1,14 @@
 var prime = require('..');
 
-describe('prime(val)', function(){
-  it('should return true for prime numbers', function(){
+describe('prime(val)', function() {
+  it('should return true for prime numbers', function() {
     prime(2).should.equal(true);
     prime(17).should.equal(true);
     prime(839).should.equal(true);
     prime(3733).should.equal(true);
     prime(999983).should.equal(true);
   });
-  it('should return false for composite numbers', function(){
+  it('should return false for composite numbers', function() {
     prime(1).should.equal(false);
     prime(4).should.equal(false);
     prime(18).should.equal(false);
