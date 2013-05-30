7b9aa88d04c5da331c3bbc601afbe113e800d1fa
Kenan Yildirim <kenan@kenany.me>
2013-05-29 23:21:37 -0400

    testling

--- test/index.js
+++ test/index.js
@@ -1,18 +1,20 @@
-var prime = require('..');
+var prime = require('../');
 
 describe('prime(val)', function() {
-  it('should return true for prime numbers', function() {
+  it('should return true for prime numbers', function(done) {
     prime(2).should.equal(true);
     prime(17).should.equal(true);
     prime(839).should.equal(true);
     prime(3733).should.equal(true);
     prime(999983).should.equal(true);
+    done();
   });
-  it('should return false for composite numbers', function() {
+  it('should return false for composite numbers', function(done) {
     prime(1).should.equal(false);
     prime(4).should.equal(false);
     prime(18).should.equal(false);
     prime(25).should.equal(false);
     prime(838).should.equal(false);
+    done();
   });
 });
