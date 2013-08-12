9d14a11f5388b1bc26a006a818e8af83cae29d62
Kenan Yildirim <kenan@kenany.me>
2013-08-12 18:24:23 -0400

    Make tests sync

--- test/index.js
+++ test/index.js
@@ -2,20 +2,18 @@ var prime = require('../');
 var should = require('should');
 
 describe('prime(val)', function() {
-  it('should return true for prime numbers', function(done) {
+  it('should return true for prime numbers', function() {
     prime(2).should.equal(true);
     prime(17).should.equal(true);
     prime(839).should.equal(true);
     prime(3733).should.equal(true);
     prime(999983).should.equal(true);
-    done();
   });
-  it('should return false for composite numbers', function(done) {
+  it('should return false for composite numbers', function() {
     prime(1).should.equal(false);
     prime(4).should.equal(false);
     prime(18).should.equal(false);
     prime(25).should.equal(false);
     prime(838).should.equal(false);
-    done();
   });
 });
